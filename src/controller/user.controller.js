ed66283101e0ed020d16816e142c53d28817a86a
vishal <dasvishal60@gmail.com>
2024-10-23 20:53:59 +0530

    ...

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -9,12 +9,8 @@ const GenerateToken = (_id, email) => {
 
 const UserSchemaValidation = z.object({
   email: z.string().email(),
-  firstName: z.string().min(2),
-  lastName: z.string().min(2),
-  phone: z.string().min(10).max(12),
-  DOB: z.string().min(2),
-  password: z.string().min(6).max(16),
-  gender: z.enum(["male", "female"]),
+  phone: z.string().min(2).max(12),
+  password: z.string().min(2).max(16),
 });
 
 const registeredUser = async (req, res) => {
